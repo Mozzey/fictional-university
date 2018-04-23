6046f436f0f077ff69a00a1eb48f4ea5458ce930
Mozzey <mbcacciano@gmail.com>
2018-04-23 06:47:48 -0400

    starting JS section

---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"Mozzey","$P$B775Xk.rQ73QyqfveuMmTYuKO7Qchn0","mozzey","dev-email@flywheel.local","","2018-04-22 15:49:45","",0,"Mozzey");
